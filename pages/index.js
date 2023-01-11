38ee2c2caf62d8dd0ebe085907fa3616c33603c4
prince214 <princeparaste78@gmail.com>
2023-01-12 01:13:25 +0530

    fix: show play container only on output

--- pages/index.js
+++ pages/index.js
@@ -73,19 +73,22 @@ const Home = () => {
               </div>
             </a>
           </div>
-          <PlaySound playText={apiOutput} />
+
           {apiOutput && (
-            <div className="output">
-              <div className="output-header-container">
-                <div className="output-header">
-                  <h3>Output</h3>
+            <>
+              <PlaySound playText={apiOutput} />
+              <div className="output">
+                <div className="output-header-container">
+                  <div className="output-header">
+                    <h3>Output</h3>
+                  </div>
+                </div>
+
+                <div className="output-content">
+                  <p>{apiOutput}</p>
                 </div>
               </div>
-
-              <div className="output-content">
-                <p>{apiOutput}</p>
-              </div>
-            </div>
+            </>
           )}
         </div>
       </div>
